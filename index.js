08ee72edd4e700145095221ca3a966781842f9b2
Craig Latta <github@netjam.org>
2017-06-25 22:07:16 +0200

    SSL support

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var childProcess = require('child_process')
 const WebSocket = require('ws')
+var fs = require('fs')
 
-const wss = new WebSocket.Server({port: 8087})
+var ws_cfg = {
+    ssl: true,
+    port: 8087,
+    ssl_key: '',
+    ssl_cert: ''
+};
+
+var processRequest = function(req, res) {
+    console.log("Request received.")
+};
+
+var httpServ = require('https');
+var app = null;
+
+app = httpServ.createServer({
+    key: fs.readFileSync(ws_cfg.ssl_key),
+    cert: fs.readFileSync(ws_cfg.ssl_cert)
+}, processRequest).listen(ws_cfg.port);
+
+var wss = new WebSocket.Server( {server: app});
+
+// const wss = new WebSocket.Server({port: 8087})
 global.wss = wss
 
 wss.on('connection', function connection(ws) {
