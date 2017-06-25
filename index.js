cc453c57265b645d7b786b14c2c286cf38c6e345
Craig Latta <github@netjam.org>
2017-06-25 14:47:24 +0200

    initial working version

---
+++ index.js
@@ -0,0 +1,20 @@
+var childProcess = require('child_process')
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({port: 8087})
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received a command')
+    var command = JSON.parse(message)
+    switch (command.verb) {
+    case 'require':
+      myRequire(ws, command.parameters);
+      break
+    default: }})})
+
+function myRequire(ws, parameters) {
+  ws.send('received require command')
+  var loadPackage = childProcess.spawn('npm', ['install', parameters.package])
+  loadPackage.on('close', function () {
+    eval(parameters.then)})}
+
